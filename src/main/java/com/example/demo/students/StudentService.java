0b493ca5ddf09d5969f5243698650256416672cf
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2023-10-04 11:32:13 +0700

    feat: get data for find all after i insert with JPA

--- src/main/java/com/example/demo/students/StudentService.java
+++ src/main/java/com/example/demo/students/StudentService.java
@@ -1,21 +1,20 @@
 package com.example.demo.students;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service //should be component but use servie to be more semantics and more for readability
 public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	@Autowired // this autoWired is same case like Injectable Controller
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository =  studentRepository;
+  }
   public List<Students> getStudents(){
-		return List.of(
-			new Students(
-			1L,
-			"Arlingga",
-			21,
-			LocalDate.of(2000, Month.JANUARY, 5),
-			"acr02@gmail.com"
-		));
+		return studentRepository.findAll();
 	}
 }
