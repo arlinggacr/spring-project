aaa2057fb74a74a04bd5cef65ea7e1d21944aef0
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2023-10-19 15:50:24 +0700

    feat: adding service for regist and delete student

--- src/main/java/com/example/demo/students/StudentService.java
+++ src/main/java/com/example/demo/students/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.students;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service //should be component but use servie to be more semantics and more for readability
+@Service //should be component but use service to be more semantics and more for readability
 public class StudentService {
 
 	private final StudentRepository studentRepository;
@@ -16,5 +17,23 @@ public class StudentService {
   }
   public List<Students> getStudents(){
 		return studentRepository.findAll();
+	}
+
+	public void addNewStudent(Students student){
+		// adding validator that email has already taken
+		Optional<Students> studentsOptional = studentRepository.findStudentsByEmail(student.getEmail());
+		if(studentsOptional.isPresent()) {
+			throw new IllegalStateException("email already taken");
+		}
+		// saving the data after validate the email
+		studentRepository.save(student);
+	}
+
+	public void deleteStudent(Long studentId) {
+		boolean dataExits = studentRepository.existsById(studentId);
+		if(!dataExits){
+			throw new IllegalStateException("Data " + studentId +" not found");
+		}
+		studentRepository.deleteById(studentId);
 	}
 }
