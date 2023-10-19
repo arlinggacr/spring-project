619213081fed75ee4687e39abda23d89ab967f28
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2023-10-19 15:49:56 +0700

    feat: adding select query find by email

--- src/main/java/com/example/demo/students/StudentRepository.java
+++ src/main/java/com/example/demo/students/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.students;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Students, Long>{
 
+    @Query("SELECT s FROM Students s WHERE s.email = ?1")
+    Optional<Students> findStudentsByEmail(String email);
 }
